68c74b5cbbf7bfb6f364e074a5c00558088f6a1b
ming197 <ming197@qq.com>
2020-08-14 14:26:35 +0800

    update 22-valid parenthese

---
+++ Algorithms/22-Valid Parentheses.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+#include<stack>
+#include<unordered_map>
+using namespace std;
+
+
+class Solution {
+public:
+    bool isValid(string s) {
+        unordered_map<char, char> dict;
+        stack<char> stk;
+        dict[')'] = '(';
+        dict['}'] = '{';
+        dict[']'] = '[';
+        int m = s.size();
+        for(int i=0; i<m; ++i){
+            char temp = s.at(i);
+            if(temp==')' || temp==']' || temp=='}'){
+                if(stk.empty()){ // 注意：要考虑stk为空的情况
+                    return false;
+                }
+                if(stk.top() == dict[temp]){
+                    stk.pop();
+                }else{
+                    return false;
+                }
+            }else{
+                if(temp=='(' || temp=='[' || temp=='{'){
+                    stk.push(temp);
+                }
+            }
+        }
+        if(stk.empty()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+};
+
+int main(){
+    Solution isvalid;
+    string s;
+    cin >> s;
+    bool ans = isvalid.isValid(s);
+    cout << ans;
+    return 0;
+}
