161dc622c4b22d68391805faea6899b4df0b9f6b
ming197 <ming197@qq.com>
2020-08-17 10:21:15 +0800

    110 balanced tree

---
+++ Algorithms/110-Balanced Binary Tree.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+using namespace std;
+
+
+// Definition for a binary tree node.
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL){}
+};
+
+
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL){
+            return true;
+        }else{
+            return abs(getHeight(root->left)-getHeight(root->right))<=1 && isBalanced(root->left) && isBalanced(root->right);
+        }
+    }
+    int getHeight(TreeNode* root){
+        
+        if(root==NULL){
+            return 0;
+        }else{
+            return 1 + max(getHeight(root->right), getHeight(root->left));
+        }
+    }
+    
+};
