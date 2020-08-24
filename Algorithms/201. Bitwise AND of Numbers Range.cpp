f7bbaab4ecc66322500ffddc2abbc0db3afd9c4c
ming197 <ming197@qq.com>
2020-08-24 23:38:41 +0800

    update

--- Algorithms/201. Bitwise AND of Numbers Range.cpp
+++ Algorithms/201. Bitwise AND of Numbers Range.cpp
@@ -7,7 +7,7 @@ class Solution {
 public:
     int rangeBitwiseAnd(int m, int n) {
         int shift = 0;
-        // 找到公共前缀
+       // 找到公共前缀
         while (m < n) {
             m >>= 1;
             n >>= 1;
