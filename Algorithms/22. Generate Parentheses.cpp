12684e146c60a550b7852105022c4dd64b4f9c25
ming197 <ming197@qq.com>
2020-08-29 23:20:45 +0800

    parentheses generate backtrack

--- Algorithms/22. Generate Parentheses.cpp
+++ Algorithms/22. Generate Parentheses.cpp
@@ -10,24 +10,23 @@ class Solution {
 public:
     vector<string> ans;
     vector<string> generateParenthesis(int n) {
-        int l = 0, r = 0;
         string temp;
-        backtrack(temp, l, r, n);
+        backtrack(temp, 0, 0, n);
         return ans;
     }
-    void backtrack(string& temp, int& l, int& r, int& n){
-        if(temp.size() == 2*n){
+    void backtrack(string& temp, int l, int r, int& n){
+        if(temp.size() == 2 * n){
             ans.emplace_back(temp);
             return;
         }
         if(l < n){
             temp.push_back('(');
-            backtrack(temp, ++l, r, n);
+            backtrack(temp, l + 1, r, n);
             temp.pop_back();
         }
-        if(r < n){
+        if(r < l){
             temp.push_back(')');
-            backtrack(temp, l, ++r, n);
+            backtrack(temp, l, r + 1, n);
             temp.pop_back();
         }
     }
