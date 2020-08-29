f98cc3c03c2f4b64e64cddec8c958d2e6b5743b4
ming197 <ming197@qq.com>
2020-08-29 10:49:31 +0800

    update

---
+++ Algorithms/214. Shortest Palindrome.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        int n = s.size();
+        string _reverse  = s;
+        reverse(_reverse.begin(), _reverse.end());
+        for(int l = n; l >= 1; l--){
+            if(_reverse.substr(n - l , l) == s.substr(0, l)) return _reverse.substr(0, n - l) + s;
+        }
+    }
+};
+
+
+int main(){
+    string s = "abcd";
+    Solution solution;
+    string ans = solution.shortestPalindrome(s);
+    cout << ans;
+
+    return 0;
+}
+
