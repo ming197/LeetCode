8d188dac4a3cfcef6f2e2372b1ad0220835ff61b
ming197 <ming197@qq.com>
2020-08-31 20:19:52 +0800

    二分

---
+++ Algorithms/33. Search in Rotated Sorted Array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int n = nums.size();
+        int l = 0, r = n - 1;
+        if(!n) return n - 1;
+        if(n == 1) return nums[0] == target ? 0 : -1;
+        while (l <= r)
+        {
+            int mid = (l + r) / 2;
+            if(nums[mid] == target) return mid;
+            if(nums[0] <= nums[mid]){
+                if(nums[0] <= target && target < nums[mid]){
+                    r = mid - 1;
+                }else{
+                    l = mid + 1;
+                }
+            }else{
+                if(nums[mid] < target && target <= nums[n - 1]){
+                    l = mid + 1;
+                }else
+                {
+                    r = mid - 1;
+                }
+                
+            }
+        }
+        return -1;
+        
+    }
+};
+
+
+int main(){
+
+    return 0;
+}
