4c3dc5a27ec461b0c5ed64bf921b4e5d297d3dfb
ming197 <ming197@qq.com>
2020-09-05 11:58:44 +0800

    update

---
+++ Algorithms/34. Find First and Last Position of Element in Sorted Array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> ans(2, -1);
+        if(target < nums[0] || target > nums[n-1]) return ans;
+        int 
+    }
+};
+
+
+int main(){
+
+    return 0;
+}
