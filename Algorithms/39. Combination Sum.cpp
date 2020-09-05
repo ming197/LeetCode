40b276dd40ba2156950e1d437c0249d1d11ced0c
ming197 <ming197@qq.com>
2020-09-05 16:12:09 +0800

    backtrack

---
+++ Algorithms/39. Combination Sum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include<vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        sort(candidates.begin(), candidates.end());
+        backtrack(candidates, target, ans, temp, 0, 0);
+        return ans;
+    }
+    void backtrack(vector<int>& candidates, int& target, vector<vector<int>>& ans, vector<int>& temp, int sum, int start){
+         if(sum == target) {
+             ans.push_back(temp);
+             return;
+         }
+         if(sum > target) return;
+         for(int i = start; i < candidates.size(); i++){
+            temp.push_back(candidates[i]);
+            backtrack(candidates, target, ans, temp, sum + candidates[i], i);
+            temp.pop_back();
+         }
+    }
+};
+
+int main(){
+    return 0;
+}
