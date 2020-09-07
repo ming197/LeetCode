73c708d66b66cfd4c0e835fbf91b315a9a938284
ming197 <ming197@qq.com>
2020-09-07 19:17:44 +0800

    dp path_num

---
+++ Algorithms/62. Unique Paths.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+
+class Solution {
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int>(n, 0));
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i - 1 >= 0 && j - 1 >= 0){
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }else{
+                    if(i - 1 >= 0) dp[i][j] = dp[i - 1][j];
+                    if(j - 1 >= 0) dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+};
+
+int main(){
+    Solution solution;
+    int ans = solution.uniquePaths(3, 2);
+    cout << ans;
+    return 0;
+}
