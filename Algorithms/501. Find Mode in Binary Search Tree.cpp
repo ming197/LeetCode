e89cc7124151ad43519eaa4b9d00ceb6d9cbf0f4
ming197 <ming197@qq.com>
2020-09-24 13:32:15 +0800

    update

---
+++ Algorithms/501. Find Mode in Binary Search Tree.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        int maxNum = 0;
+        
+    }
+};
